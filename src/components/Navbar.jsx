d11d67a2f8aab08f45da00a31bb6dd1a48e8060e
rishavtiwar22 <rishav@navgurukul.org>
2025-10-09 13:29:41 +0530

    fix - Level spacing in center

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
       </button>
 
       {/* Center - Level buttons (Desktop) */}
-      <div className="hidden md:flex items-center justify-center flex-1 max-w-lg mx-2 sm:mx-4">
+      <div className="hidden md:flex items-center justify-center flex-1 mx-2 sm:mx-4 ml-12 md:ml-40 lg:ml-58">
         <div className="flex items-center space-x-1 sm:space-x-2 md:space-x-3 lg:space-x-4">
           {[1, 2, 3, 4, 5].map((level) => (
             <button
@@ -43,7 +43,7 @@ function Navbar() {
               <img 
                 src={`/src/assets/${level}.svg`} 
                 alt={`Level ${level}`}
-                className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-16 lg:h-16"
+                className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-18 lg:h-18"
               />
             </button>
           ))}
@@ -57,7 +57,7 @@ function Navbar() {
           <img 
             src="/src/assets/speaker.svg" 
             alt="Speaker/Guide"
-            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-10 md:h-10 lg:w-12 lg:h-12"
           />
         </button>
 
@@ -75,7 +75,7 @@ function Navbar() {
           <img 
             src="/src/assets/user.svg" 
             alt="User/Settings"
-            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-10 md:h-10 lg:w-12 lg:h-12"
           />
         </button>
       </div>
@@ -101,7 +101,7 @@ function Navbar() {
                   <img 
                     src={`/src/assets/${level}.svg`} 
                     alt={`Level ${level}`}
-                    className="w-8 h-8"
+                    className="w-14 h-14"
                   />
                 </button>
               ))}
